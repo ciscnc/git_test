70af19b7a38a3839406788448e9c3ac04e585edf
ciscnc <2165712815@qq.com>
2025-09-24 20:24:42 +0800

    第三次提交

--- git_test.c
+++ git_test.c
@@ -2,6 +2,12 @@
 
 int main(){
 
-    printf("This is git test!\r\n");
+    printf("This is git test!\r\n"); 
+
+    //测试版本回滚 git reset --hard 版本号
+    //soft和hard的区别：
+    //
+    //
+
     return 0;
 }
